3c98b2d2ad48ed50bf921e0129d51304275aaab9
gahyeon <rkgus758@gmail.com>
2022-10-25 16:12:30 +0900

    페이지 불러오기 스크롤, 넘버

--- src/main/java/com/stussy/stussyclone20220930gahyeon/controller/CollectionsController.java
+++ src/main/java/com/stussy/stussyclone20220930gahyeon/controller/CollectionsController.java
@@ -9,7 +9,8 @@ public class CollectionsController {
 
     @GetMapping("/collections/{category}")
     public String loadCollections(@PathVariable String category){
-        return "product/collections_number";
+
+        return "product/collections_scroll";
     }
 
 }
--- src/main/resources/static/js/product/collections-number.js
+++ src/main/resources/static/js/product/collections-number.js
@@ -55,6 +55,7 @@ class PageNumber{
         this.createPreButton();
         this.createNumberButtons();
         this.createNextButton();
+        this.addPageButtonEvent();
     }
 
     createPreButton(){
@@ -85,6 +86,30 @@ class PageNumber{
             `;
         }
     }
+
+    addPageButtonEvent(){
+        const pageButtons = this.#pageNumberList.querySelectorAll("li");
+        pageButtons.forEach(button => {
+            button.onclick = () => {
+                if(button.textContent == "<"){
+                    const nowPage = CollectionsService.getInstance().collectionsEntity.page;
+                    CollectionsService.getInstance().collectionsEntity.page = Number(nowPage) - 1;
+                    CollectionsService.getInstance().loadCollections();
+                }else if(button.textContent == ">"){
+                    const nowPage = CollectionsService.getInstance().collectionsEntity.page;
+                    CollectionsService.getInstance().collectionsEntity.page = Number(nowPage) + 1;
+                    CollectionsService.getInstance().loadCollections();
+                }else{
+                    const nowPage = CollectionsService.getInstance().collectionsEntity.page;
+                    if(button.textContent != nowPage){
+                        CollectionsService.getInstance().collectionsEntity.page = button.textContent;
+                        CollectionsService.getInstance().loadCollections();
+                    }
+                }
+            }
+
+        });
+    }
 }
 
 class CollectionsService{
@@ -104,9 +129,38 @@ class CollectionsService{
 
     loadCollections() {
         const responseData = CollectionsApi.getInstance().getCollections(this.collectionsEntity.page);
-        this.collectionsEntity.totalCount = responseData[0].productTotalCount;
-        new PageNumber(this.collectionsEntity.page, this.collectionsEntity.totalCount);
+        console.log(responseData);
+        if(responseData.length > 0){
+            this.collectionsEntity.totalCount = responseData[0].productTotalCount;
+
+            new PageNumber(this.collectionsEntity.page, this.collectionsEntity.totalCount);
+            this.getCollections(responseData);
+        }else{
+            alert("해당 카테고리에 등록된 상품 정보가 없습니다.");
+            location.href = "/collections/all"
+        }
         
+    }
+
+    getCollections(responseData){
+        const collectionProducts = document.querySelector(".collection-products");
+        collectionProducts.innerHTML = ``;
+
+        responseData.forEach(product => {
+            collectionProducts.innerHTML += `
+                <li class="collection-product">
+                    <div class="product-img">
+                        <img src="/static/images/product/acc1.png">
+                    </div>
+                    <div class="product-name">
+                        ${product.productName}
+                    </div>
+                    <div class="product-price">
+                        ₩${product.productPrice}
+                    </div>
+                </li>
+            `;
+        })
     }
 
 
